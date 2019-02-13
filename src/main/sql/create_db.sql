47164cd98c0a02da4b94f64f9893f0d99500323f
alex <qwert2603@mail.ru>
2019-02-13 15:25:33 +0700

    create tables "users", "tokens"

--- src/main/sql/create_db.sql
+++ src/main/sql/create_db.sql
@@ -1,8 +1,10 @@
-create sequence record_change_id_seq
-;
+create sequence record_change_id_seq;
 
-create sequence category_change_id_seq
-;
+alter sequence record_change_id_seq owner to postgres;
+
+create sequence category_change_id_seq;
+
+alter sequence category_change_id_seq owner to postgres;
 
 create table record_types
 (
@@ -10,16 +12,42 @@ create table record_types
 		constraint record_types_pkey
 			primary key,
 	name varchar(64) not null
-)
-;
+);
+
+alter table record_types owner to postgres;
 
 create unique index record_types_id_uindex
-	on record_types (id)
-;
+	on record_types (id);
 
 create unique index record_types_name_uindex
-	on record_types (name)
-;
+	on record_types (name);
+
+create table users
+(
+	id serial not null
+		constraint users_pk
+			primary key,
+	login varchar(64) not null
+);
+
+alter table users owner to postgres;
+
+create table record_categories
+(
+	uuid text not null
+		constraint record_kinds_pkey
+			primary key,
+	name varchar(64) not null,
+	record_type_id integer not null
+		constraint record_kinds_record_types_id_fk
+			references record_types,
+	change_id integer default nextval('category_change_id_seq'::regclass) not null,
+	user_id integer not null
+		constraint record_categories_users_id_fk
+			references users
+);
+
+alter table record_categories owner to postgres;
 
 create table records
 (
@@ -31,40 +59,46 @@ create table records
 	value integer not null,
 	deleted boolean default false not null,
 	time time,
-	record_category_uuid text not null,
+	record_category_uuid text not null
+		constraint records_record_categories_uuid_fk
+			references record_categories,
 	change_id integer default nextval('record_change_id_seq'::regclass) not null
-)
-;
+);
+
+alter table records owner to postgres;
 
 create unique index records_uuid_uindex
-	on records (uuid)
-;
-
-create table record_categories
-(
-	uuid text not null
-		constraint record_kinds_pkey
-			primary key,
-	name varchar(64) not null,
-	record_type_id integer not null
-		constraint record_kinds_record_types_id_fk
-			references record_types,
-	change_id integer default nextval('category_change_id_seq'::regclass) not null
-)
-;
+	on records (uuid);
 
 create unique index record_kinds_uuid_uindex
-	on record_categories (uuid)
-;
+	on record_categories (uuid);
 
 create unique index record_kinds_name_record_type_id_uindex
-	on record_categories (name, record_type_id)
-;
+	on record_categories (name, record_type_id, user_id);
 
-alter table records
-	add constraint records_record_categories_uuid_fk
-		foreign key (record_category_uuid) references record_categories
-;
+create index record_categories_user_id_index
+	on record_categories (user_id);
+
+create unique index users_id_uindex
+	on users (id);
+
+create unique index users_login_uindex
+	on users (login);
+
+create table tokens
+(
+	user_id integer not null
+		constraint tokens_users_id_fk
+			references users,
+	token_hash varchar(64) not null
+);
+
+alter table tokens owner to postgres;
+
+create index tokens_token_hash_index
+	on tokens (token_hash);
+
+
 
 INSERT INTO record_types (id, name) VALUES (1, 'расход'), (2, 'доход');
 
